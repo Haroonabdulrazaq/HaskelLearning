b17f057268089ea26aed5a5f4835480c79ec70dd
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-08-18 21:58:46 +0100

    Add list method

--- hello.hs
+++ hello.hs
@@ -1,23 +1,33 @@
-name :: String
-name = "Doudley"
-
-numOfGifts :: Int
-numOfGifts = 36
-
-myLetters :: Char
-myLetters = 'a'
-
-doudleyGpa :: Double
-doudleyGpa = 3.71
-
+scores :: [Int]
+scores = [73,83, 0]
 main = do
-  putStrLn "Welcome to Haskell Calculator"
-  putStrLn "Enter first number"
-  fnum <- getLine
-  putStrLn "Enter Second number"
-  snum <- getLine
 
-  let newFnum = read fnum::Int  -- Type Casting
-  let newSnum = read snum::Int  -- Type Casting
+  print (head scores)
+  print (tail scores)
+  -- print (init scores)
+  -- print (last scores)
 
-  putStrLn("The result is: " ++ show (newFnum + newSnum))
+
+
+-- name :: String
+-- name = "Doudley"
+
+-- numOfGifts :: Int
+-- numOfGifts = 36
+
+-- myLetters :: Char
+-- myLetters = 'a'
+
+-- doudleyGpa :: Double
+-- doudleyGpa = 3.71
+
+  -- putStrLn "Welcome to Haskell Calculator"
+  -- putStrLn "Enter first number"
+  -- fnum <- getLine
+  -- putStrLn "Enter Second number"
+  -- snum <- getLine
+
+  -- let newFnum = read fnum::Int  -- Type Casting
+  -- let newSnum = read snum::Int  -- Type Casting
+
+  -- putStrLn("The result is: " ++ show (newFnum + newSnum))
