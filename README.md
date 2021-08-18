57641eb4f5a93abf15a99ffa31c39fb4f1da0e60
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-08-18 12:07:46 +0100

    Add README run Instructions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Haskel Learning
 Learning Haskel to solve some challenges
+
+
+## To run 
+- cd into the project directory
+- run this command to load file into haskel platform(Unix) ``` ghc -o hello hello.hs ```
+- then run ./hello to run the file
+
+Note: Make sure to relaod after every file changes
--- hello.hs
+++ hello.hs
@@ -1,4 +1,6 @@
 main = do
-  putStrLn (" Doudley recieves 36 gift for her birthday")
-  putStrLn (" 3gifts Doudly though out loud, but i received 17 last year")
-  putStrLn ("Obviously Doudley wanted more because she is a spoilt child")
+  let name = "Doudley"
+  let extraname = "Haroon"
+  putStrLn (name++ " recieves 36 gift for her birthday")
+  putStrLn (" 36 gifts "++ name ++" thought out loud, but i received 17 last year")
+  putStrLn (" Obviously, "++ name ++" wanted more because she is a spoilt child")
